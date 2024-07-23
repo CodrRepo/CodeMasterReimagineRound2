f4120da757d7e442ab815f7933f015547405d070
Nitesh <email.niteshkr@gmail.com>
2024-07-23 18:22:01 +0530

    added other categories section and changed purchase instruction animation

--- script.js
+++ script.js
@@ -38,6 +38,12 @@ function applyLocomotive(){
 
 applyLocomotive();
 
+let gameCardContainer = document.querySelector(".free-gc");
+let hero = document.querySelector(".hero");
+let heroItems = document.querySelectorAll(".hero-item");
+let gameVideo = document.querySelector(".game-video");
+let otherCategories = document.querySelector(".other-categories");
+
 
 let freeGameCardData = [
   {
@@ -74,11 +80,54 @@ let freeGameCardData = [
   },
 ];
 
-let gameCardContainer = document.querySelector(".free-gc");
-let hero = document.querySelector(".hero");
-let heroItems = document.querySelectorAll(".hero-item");
-let card = document.querySelectorAll(".item");
-let gameVideo = document.querySelector(".game-video");
+let otherCategoriesData = [
+  {
+    heading: "Discover New",
+    gameList: [{
+      title: "Hell Drivers II",
+      image: "./Asserts/helldivers.webp",
+    },
+    {
+      title: "the last of us part I",
+      image: "./Asserts/the-last-of-us-part-1.webp",
+    },
+    {
+      title: "Horizon Forbidden West™",
+      image: "./Asserts/horizon.avif",
+    }],
+  },
+  {
+    heading: "Top Upcomings",
+    gameList: [{
+      title: "Black Myth: Wukong",
+      image: "./Asserts/game2.jpg",
+    },
+    {
+      title: "Call of Duty®: Black Ops 6",
+      image: "./Asserts/call of duty bo vi.avif",
+    },
+    {
+      title: "Gta VI",
+      image: "./Asserts/gta vi.jpg_large",
+    }],
+  },
+  {
+    heading: "Most played",
+    gameList: [{
+      title: "gta v",
+      image: "./Asserts/gta v.jpg",
+    },
+    {
+      title: "UNCHARTED: Legacy of Thieves",
+      image: "./Asserts/uncharted.webp",
+    },
+    {
+      title: "god of war",
+      image: "./Asserts/god-of-war.webp",
+    }],
+  },
+];
+
 
 // Sperate Text into span
 function splitText(textClassName) {
@@ -149,8 +198,72 @@ heroItems.forEach((heroItem, index) => {
   });
 });
 
+
+// ------------------------- OTHER LINKS ------------------------
+otherCategoriesData.forEach((element, index)=>{
+  let headingArr = element.heading.split(" ");
+  otherCategories.innerHTML += `<div class="flex flex-col md:flex-row text-white items-center h-[50vh] w-full md:h-[30vh] lg:h-[35vh] lg:w-[85%] m-auto mt-16 md:mt-0">
+                <div class="text-5xl md:text-4xl lg:text-3xl 2xl:text-5xl flex flex-col items-center md:items-start leading-[3.2rem] md:leading-none text-white mb-2  w-[30%]">
+                  <h2>${headingArr[0]}<h2/>
+                  <h2>${headingArr[1]}<h2/>
+                </div>
+                <div class="oGameListContainer flex justify-between mt-3 md:mt-0  h-full w-full md:w-[70%] lg:pr-[10vh] gap-2 overflow-auto">
+                    <div class="w-full md:w-[50vh] lg:w-[33.33%] shrink-0 p-2 relative item category h-full flex flex-col justify-center items-center">
+                        <img src="${element.gameList[0].image}" class="absolute h-full w-full left-0 right-0 object-cover object-top" alt="">
+                        <h2 class="text-xl 2xl:text-3xl text-center">${element.gameList[0].title}</h2>
+                    </div>
+
+                    <div class="w-full md:w-[50vh] lg:w-[33.33%] shrink-0 p-2 relative item category h-full flex flex-col justify-center items-center">
+                        <img src="${element.gameList[1].image}" class="absolute h-full w-full left-0 right-0 object-cover  lg:opacity-0 object-top" alt="">
+                        <h2 class="text-xl 2xl:text-3xl text-center">${element.gameList[1].title}</h2>
+                    </div>
+
+                    <div class="w-full md:w-[50vh] lg:w-[33.33%] shrink-0 p-2 relative item category h-full flex flex-col justify-center items-center">
+                        <img src="${element.gameList[2].image}" class="absolute h-full w-full left-0 right-0 object-cover  lg:opacity-0 object-top" alt="">
+                        <h2 class="text-xl 2xl:text-3xl text-center">${element.gameList[2].title}</h2>
+                    </div>
+    
+                </div>
+            </div>`
+})
+
+let categories = document.querySelectorAll(".category");
+window.innerWidth > 830 && (categories.forEach((element, index)=>{
+  element.addEventListener('mouseover', ()=>{
+
+    categories.forEach((category, subIndex)=>{
+      
+      if(subIndex!= index){
+        gsap.to(category.querySelector("img"), {
+          onStart: ()=>{
+            gsap.to(category.querySelector("h2"),{
+              opacity: 1,
+              duration: 0.2,
+            })
+          },
+
+          opacity: 0,
+          duration: 0.3,
+        })
+      }
+    });
+
+    gsap.to(element.querySelector("img"), {
+      onStart: ()=>{
+        gsap.to(element.querySelector("h2"),{
+          opacity: 0,
+          duration: 0.2,
+        })
+      },
+      opacity: 1,
+      duration: 0.3,
+    })
+  })
+}))
+
 // ----------- ITEM SECTION ------------------------
 
+let card = document.querySelectorAll(".item");
 card.forEach((element, index) => {
   element.addEventListener("mousemove", (e) => {
     const rect = element.getBoundingClientRect();
@@ -240,9 +353,9 @@ gsap.to(".trending-item", {
 // --------------- FREE GAME CARDS SECTION --------------------
 
 freeGameCardData.forEach((card, index) => {
-  gameCardContainer.innerHTML += `<div class="f-game-card rotate-[${card.rotate}deg] origin-bottom-${card.origin} absolute top-0 left-[50%] -translate-x-[50%] shrink-0">
+  gameCardContainer.innerHTML += `<div class="f-game-card ${index == 0 && 'backdrop-blur-md'} rotate-[${card.rotate}deg] origin-bottom-${card.origin} absolute top-0 left-[50%] -translate-x-[50%] shadow-[0_8px_12px_-5px_rgba(0,0,0,0.8)] shrink-0">
                         <div class="item relative h-[35vh] w-[35vh] md:h-[30vh] lg:h-[58vh]  md:w-[30vh] lg:w-[60vh] flex justify-center items-center border-[1px] border-[#575757]">
-                            <img class="absolute ${index == 0 && 'hidden'} top-0 left-0 h-full w-full object-cover object-center z-10" src="${card.image}" alt="">
+                            <img class="absolute ${index == 0 && 'hidden backdrop-blur-lg'} top-0 left-0 h-full w-full object-cover object-center z-10" src="${card.image}" alt="">
                             <div class="absolute bottom-0 left-0 h-[100%] w-full z-20 bg-gradient-to-t from-[#000000] to-[#00000000]"></div>
                             <h2 class="text-white text-[3vh] md:text-[3.9vh] xl:text-[4.3vh] text-center z-30 px-2">${card.title}</h2>
                         </div>
@@ -311,12 +424,12 @@ let purchaseHeading = document.querySelectorAll(".purchase-heading");
 
 purchaseHeading.forEach((heading, index) => {
   gsap.to(heading, {
-    transform: `translateX(120%) ${index == 0 ? 'rotate(-10deg)' : 'rotate(10deg)'}`,
+    transform: `translateX(-10%)`,
     scrollTrigger: {
       trigger: ".purchase-heading-container",
       scroller: "#main",
-      start: `top ${window.innerWidth > 768? '90%}': '80%'}`,
-      end: `top ${window.innerWidth > 768? '-350%}': '-130%'}`,
+      start: `top ${window.innerWidth > 768? '105%}': '105%'}`,
+      end: `top ${window.innerWidth > 768? '-70%}': '-70%'}`,
       scrub: 1,
       duration: 10,
     }
@@ -324,16 +437,38 @@ purchaseHeading.forEach((heading, index) => {
 )
 });
 
-gsap.to(gameVideo, {
-  scrollTrigger: {
-    trigger: gameVideo,
-    scroller: "#main",
-    start: `top 120%`,
-    end: `top -100%`,
-    onEnter: () => {gameVideo.play()},
-      onLeave: () => gameVideo.pause(),
-      onEnterBack: () => gameVideo.play(),
-      onLeaveBack: () => gameVideo.pause(),
 
-  },
-})
+let instructions = document.querySelectorAll(".instructions");
+
+instructions.forEach((instruction, index)=>{
+  gsap.from(instruction.querySelectorAll(".heading-reveal"), {
+    y: 150,
+    opacity: 0,
+    duration: 0.5,
+    scrollTrigger: {
+      trigger: instruction,
+      scroller: "#main",
+      start: "top 85%",
+      end: "top 10%",  
+      ease: "power4.out",
+    }
+  })
+});
+
+
+// gsap.to(gameVideo, {
+//   scrollTrigger: {
+//     trigger: gameVideo,
+//     scroller: "#main",
+//     start: `top 120%`,
+//     end: `top -100%`,
+//     onEnter: () => {gameVideo.play()},
+//       onLeave: () => gameVideo.pause(),
+//       onEnterBack: () => gameVideo.play(),
+//       onLeaveBack: () => gameVideo.pause(),
+
+//   },
+// })
+
+
+
